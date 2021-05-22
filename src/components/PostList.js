35637f5b82294a424bd35621986e528e22fd06f6
Aman Raj <amanranker@gmail.com>
2021-05-22 22:45:01 +0530

    Initial commit

---
+++ src/components/PostList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PostList extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             posts: [],
+             errors: ''
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                List of posts
+                {
+                    this.state.posts.length ?
+                    this.state.posts.map(post => 
+                        <div key={post.id}>{post.title}</div>) :
+                        <div>{this.state.errors}</div>
+                }
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response)
+                this.setState({
+                    posts: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                this.setState({
+                    errors: 'Something wrong Aman'
+                })
+            })
+    }
+}
+
+export default PostList
